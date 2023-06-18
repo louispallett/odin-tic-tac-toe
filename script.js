c147b3d20b67bdb09af8489dcd377e8d2d6bc68d
louispallett <louis.pallett@outlook.com>
2023-06-18 11:16:20 +0100

    Initial Commit. Created gameBoard() to create grid layout

---
+++ script.js
@@ -0,0 +1,26 @@
+//Tic Tac Toe instructions: https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
+
+const gameBoard = (() => {
+    const gameBoard = []
+    const gridContainer = document.querySelector(".grid-container");
+    const createBoard = () => {
+        for(let i = 0; i < 9; i++) {
+        let gridItem = document.createElement("div");
+        gridContainer.appendChild(gridItem);
+    }
+    }
+    return {createBoard}
+})();
+
+gameBoard.createBoard();
+
+
+
+const player = (name, marker) => {
+    const capitalize = () => name.toUpperCase();
+    const sayName = () => console.log(`${capitalize()} is playing as '${marker}'!`);
+    return {sayName};
+}
+
+const player1 = player("Louis", "X");
+player1.sayName();
