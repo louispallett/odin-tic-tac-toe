f7ff1525cbf43d43ec284e2d6f259f32af4badf3
louispallett <louis.pallett@outlook.com>
2023-06-19 18:47:07 +0100

    Created play() which appends player.marker to gameBoard.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Tic Tac Toe instructions: https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
 
 const gameBoard = (() => {
-    const gameBoard = []
+    const gameBoard = [];
     const gridContainer = document.querySelector(".game-container");
     const createBoard = () => {
         for(let i = 0; i < 9; i++) {
         let gridItem = document.createElement("div");
-        gridItem.onclick = function() {play([i])};
+        gridItem.onclick = function() {play([i], this)};
         gameBoard.push(gridItem);
         console.log(gameBoard);
         gridContainer.appendChild(gameBoard[i]);
@@ -18,14 +18,15 @@ const gameBoard = (() => {
 gameBoard.createBoard();
 
 //This will change!
-function play(item) {
+function play(item, box) {
+    box.innerHTML = `<div>${player1.marker}</div>`
     console.log(`You clicked box ${item}`);
 } 
 
 const player = (name, marker) => {
     const capitalize = () => name.toUpperCase();
     const sayName = () => console.log(`${capitalize()} is playing as '${marker}'!`);
-    return {sayName};
+    return {sayName, marker};
 }
 
 const player1 = player("Louis", "X");
