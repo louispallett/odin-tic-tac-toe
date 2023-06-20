ab2c1726e290ed4031e7b620fa6220dbfc4cc7ff
louispallett <louis.pallett@outlook.com>
2023-06-20 20:50:56 +0100

    checkWinner() and play() to determine winner

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ const gameBoard = (() => {
         }
     }    
 
+    const checkWinner = () => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for(const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (
+                gameBoardArr[a].textContent !== "" &&
+                gameBoardArr[a].textContent === "X" &&
+                gameBoardArr[a].textContent === gameBoardArr[b].textContent &&
+                gameBoardArr[a].textContent === gameBoardArr[c].textContent
+            ) {
+                return "x-true";
+            } else if(
+                gameBoardArr[a].textContent !== "" &&
+                gameBoardArr[a].textContent === "O" &&
+                gameBoardArr[a].textContent === gameBoardArr[b].textContent &&
+                gameBoardArr[a].textContent === gameBoardArr[c].textContent
+            ) {
+                return "o-true";                
+            }
+        }
+
+        return false;
+    }
+
     const timesRun = []; //In order to stop the play() create a new array each time, this needs to be outside the function
 
     const play = ((item, box) => {
@@ -34,8 +63,15 @@ const gameBoard = (() => {
         } else { //Remove later
             console.log("Nope!");
         }   
+
+        if (gameBoard.checkWinner() === "x-true") {
+            console.log("X wins!");
+        } else if (gameBoard.checkWinner() === "o-true") {
+            console.log("O wins!")
+        }
     })
-    return {gameBoardArr, createBoard, play, timesRun}
+
+    return {gameBoardArr, createBoard, play, timesRun, checkWinner}
 })();
 
 function play() {
@@ -54,26 +90,3 @@ player1.sayName();
 
 const player2 = player("Rhianna", "O");
 player2.sayName();
-
-// The logic of winning is as follows:
-
-// Combinations:
-
-// 0 1 2
-// 3 4 5
-// 6 7 8
-
-// 0 3 6
-// 1 4 7
-// 2 5 8
-
-// 0 4 8
-// 2 4 6
-
-// const winner = ((p1, p2, p3) => {
-//     function checkwin() {
-//         console.log("Huh");
-//     }
-//     const newItem = gameBoard.gameBoardArr;
-//     return {newItem, checkwin}
-// })();
