ed3978574052c07c1fa2db3a05f2d296ea18b6f1
louispallett <louis.pallett@outlook.com>
2023-06-25 22:15:49 +0100

    Added winner()

--- game-ai.js
+++ game-ai.js
@@ -45,8 +45,63 @@ const play = (() => {
             }
         }
     }
-
     return{startPlay, play}
+})();
+
+const winner = (() => {
+    
+    const checkWinner = () => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical
+            [0, 4, 8], [2, 4, 6] //Diagon Alley!
+        ];
+
+        for(const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (
+                gameBoard.gameBoardArr[a].textContent !== "" &&
+                gameBoard.gameBoardArr[a].textContent === "X" &&
+                gameBoard.gameBoardArr[a].textContent === gameBoard.gameBoardArr[b].textContent &&
+                gameBoard.gameBoardArr[a].textContent === gameBoard.gameBoardArr[c].textContent
+            ) {
+                gameBoard.gameBoardArr[a].classList.add("winning-combination");
+                gameBoard.gameBoardArr[b].classList.add("winning-combination");
+                gameBoard.gameBoardArr[c].classList.add("winning-combination");
+                return "x-true";
+            } else if(
+                gameBoard.gameBoardArr[a].textContent !== "" &&
+                gameBoard.gameBoardArr[a].textContent === "O" &&
+                gameBoard.gameBoardArr[a].textContent === gameBoard.gameBoardArr[b].textContent &&
+                gameBoard.gameBoardArr[a].textContent === gameBoard.gameBoardArr[c].textContent
+            ) {
+                gameBoard.gameBoardArr[a].classList.add("winning-combination");
+                gameBoard.gameBoardArr[b].classList.add("winning-combination");
+                gameBoard.gameBoardArr[c].classList.add("winning-combination");
+                return "o-true";                
+            }
+        }
+        return false;
+    }
+
+    const result = () => {
+        //Return when checkWinner() is true
+        if (checkWinner() === "x-true") {
+            console.log(`${player1.name} wins!`);
+            // resultAnounceWin(player1.name);
+            // stopPlay();
+        } else if (checkWinner() === "o-true") {
+            console.log(`${player2.name} wins!`);
+            // resultAnounceWin(player2.name);
+            // stopPlay();
+        } else if (timesRun.length === 9) {
+            console.log("Tie!");
+            // resultAnounceTie();
+            // stopPlay();
+        }
+    }
+
+    return{checkWinner, result}
 })();
 
 const createPlayer = (name, marker) => {
